c1e0868d622d48c4edeb22b604bb3a17882e8683
tian.xia <tian.xia@tongfudun.com>
2017-06-07 13:28:25 +0800

    new truffle 3.0

--- truffle.js
+++ truffle.js
@@ -1,23 +1,9 @@
 module.exports = {
-  build: {
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ],
-    "images/": "images/"
-  },
-  rpc: {
-    host: "localhost",
-    port: 8545
-  },
-    networks: {
-	"alloc": {
-	  network_id: 31415926, // custom private network
-	  host:"localhost",
-	  port: 8545
-	},
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
   }
 };
